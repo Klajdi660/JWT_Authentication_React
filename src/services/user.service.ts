92fbd9511b3431dfc012a902089b1f8dd3be637c
Klajdi660 <klajdixhafkollari36@gmail.com>
2024-11-04 21:56:51 +0100

    fix2222

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -143,7 +143,7 @@ export const signToken = async (
       "accessTokenPrivateKey",
       {
         // expiresIn: `${rememberRefreshTokenExpiresIn}d`,
-        expiresIn: "1m",
+        expiresIn: "1d",
       }
     );
 
