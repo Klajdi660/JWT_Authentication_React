38e09317004f14a783e780c67ccc07b60b9d8262
Klajdi Xhafkollari <klajdixhafkollari36@gmail.com>
2025-02-06 17:09:52 +0100

    chngs1

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -181,7 +181,9 @@ export const loginHandler = async (
   if (!user) {
     return res.json({
       error: true,
-      message: "User is not Registered with us, please Sign Up to continue.",
+      // message: "User is not Registered with us, please Sign Up to continue.",
+      message:
+        "Sorry, your password was incorrect. Please double-check your password.",
     });
   }
 
