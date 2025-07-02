59928db0910596e99c570dd1699a6df3ed810128
Klajdi Xhafkollari <klajdixhafkollari36@gmail.com>
2025-07-02 16:46:35 +0200

    dt_02_07_25_chngs_4

--- src/utils/sms.ts
+++ src/utils/sms.ts
@@ -8,35 +8,34 @@ const { smsAccoutSId, smsAuthToken, smsPhoneNumber, smsApiKey, smsFrom } =
   config.get<SMSConfigs>("providersConfigs.sms");
 
 const client = twilio(smsAccoutSId, smsAuthToken);
-const mailerSend = new MailerSend({
-  apiKey: smsApiKey,
-});
+// const mailerSend = new MailerSend({
+//   apiKey: smsApiKey,
+// });
 
 export const sendSms = async (message: string, phoneNumber: string) => {
-  //   return client.messages
-  //     .create({
-  //       body: message,
-  //       from: smsPhoneNumber,
-  //       to: phoneNumber,
-  //     })
-  //     .catch((error) => {
-  //       log.error(
-  //         JSON.stringify({ action: "send_sms_catch", message: error.message })
-  //       );
-  //     });
-
-  try {
-    const smsParams = new SMSParams()
-      .setFrom(smsFrom)
-      .setTo([phoneNumber])
-      .setText(message);
-    return mailerSend.sms.send(smsParams);
-  } catch (error: any) {
-    log.error(
-      JSON.stringify({
-        action: "send_sms_catch",
-        message: error.response?.data || error.message || error,
-      })
-    );
-  }
+  return client.messages
+    .create({
+      body: message,
+      from: smsPhoneNumber,
+      to: phoneNumber,
+    })
+    .catch((error) => {
+      log.error(
+        JSON.stringify({ action: "send_sms_catch", message: error.message })
+      );
+    });
+  //   try {
+  //     const smsParams = new SMSParams()
+  //       .setFrom(smsFrom)
+  //       .setTo([phoneNumber])
+  //       .setText(message);
+  //     return mailerSend.sms.send(smsParams);
+  //   } catch (error: any) {
+  //     log.error(
+  //       JSON.stringify({
+  //         action: "send_sms_catch",
+  //         message: error.response?.data || error.message || error,
+  //       })
+  //     );
+  //   }
 };
